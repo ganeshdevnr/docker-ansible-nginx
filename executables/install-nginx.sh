8acd60135023057df059bf9c264aa8ed18bd99fa
ganeshdevnr <ganesh.dev.nr@gmail.com>
2025-02-14 18:37:12 +0530

    install load balancer

--- executables/install-nginx.sh
+++ executables/install-nginx.sh
@@ -3,7 +3,6 @@
 echo "Installing NGINX..."
 sudo apt update
 sudo apt install -y nginx
-echo "<h1>Welcome to NGINX on $(hostname)</h1>" | sudo tee /var/www/html/index.html
 sudo systemctl enable nginx
 sudo systemctl restart nginx
 
